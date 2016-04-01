9d3428d489b7ba379ae406636f724b7a45656a09
Luke Ittycheria <luke.ittycheria@gmail.com>
2016-04-01 13:53:28 -0400

    This is the test case file

---
+++ psRunner.py
@@ -0,0 +1,90 @@
+#Author: Luke Ittycheria
+#Date: 3/28/16
+#Program: Test Cases of Problem Set 0 Functions
+
+import ps0
+
+print("")
+print("")
+print("")
+
+#TEST CASES BELOW:
+
+#Function number 0:
+
+print("Test Cases for Function number 0:")
+print("The Number 8 is: " + str(ps0.is_even(8)))
+print("The Number 43 is: " + str(ps0.is_even(43)))
+print("The Number 1 is: " + str(ps0.is_even(1)))
+print("")
+
+
+#Function Number 1:
+
+print("Test Cases for Function number 1:")
+print("The Amount of Digits in the number 56 is: " + str(ps0.num_digits(56)))
+print("The Amount of Digits in the number 376 is: " + str(ps0.num_digits(376)))
+print("The Amount of Digits in the number 4 is: " + str(ps0.num_digits(4)))
+print("")
+
+
+#Function Number 2:
+
+print("Test Cases for Function number 2: ")
+print("The Sum of the Digits in the number 25 is: " + str(ps0.sum_digits(25)))
+print("The Sum of the Digits in the number 876 is: " + str(ps0.sum_digits(876)))
+print("The Sum of the Digits in the number 2 is: " + str(ps0.sum_digits(2)))
+print("")
+
+
+#Function Number 3:
+
+print("Test Cases for Function number 3: ")
+print("The Sum of all the numbers less than 5 is: " + str(ps0.sum_less_digits(5)))
+print("The Sum of all the numbers less than 10 is: " + str(ps0.sum_less_digits(10)))
+print("The Sum of all the numbers less than 30 is: " + str(ps0.sum_less_digits(30)))
+print("")
+
+
+#Function Number 4: #HOW MANY TEST CASES??
+print("Test Cases for Function number 4: ")
+print("The Factorial of the number 5 is: " + str(ps0.factorial(5)))
+print("The Factorial of the number 3 is: " + str(ps0.factorial(3)))
+print("The Factorial of the number 10 is: " + str(ps0.factorial(10)))
+print("The Factorial of the number 0 is: " + str(ps0.factorial(0)))
+print("")
+
+
+
+#Function Number 5:
+print("Test Cases for Function number 5: ")
+print("Is 5 a factor of 10? " + str(ps0.factor(10,5)))
+print("Is 6 a factor of 10? " + str(ps0.factor(10,6)))
+print("Is 8 a factor of 437? " + str(ps0.factor(437,8)))
+print("")
+
+
+#Function Number 6: #HOW MANY TETS CASES???
+print("Test Cases for Function number 6: ")
+print("Is the number 8 prime? " + str(ps0.primeNum(8)))
+print("Is the number 431 prime? " + str(ps0.primeNum(431)))
+print("Is the number 7 prime? " + str(ps0.primeNum(7)))
+print("Is the number 2 prime? " + str(ps0.primeNum(2)))
+print("")
+
+
+#Function Number 7
+print("Test Cases for Function number 7: ")
+print("Is the number 6 perfect? " + str(ps0.isperfect(6)))
+print("Is the number 7 perfect? " + str(ps0.isperfect(7)))
+print("")
+
+
+#Function Number 8
+print("Test Cases for Function number 8: ")
+print("Does the sum of digits of the number 20 divide evenly into itself? " +str(ps0.sumfactor(20)))
+print("Does the sum of digits of the number 35 divide evenly into itself? " +str(ps0.sumfactor(35)))
+print("Does the sum of digits of the number 135 divide evenly into itself? " +str(ps0.sumfactor(135)))
+print("")
+
+
